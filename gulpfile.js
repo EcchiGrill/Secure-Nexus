2e76e7608f6cfc372bf7fe4ab235d0bff3d1157e
Dmitry <dmitry.oborsky@gmail.com>
2024-06-10 21:26:24 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require( 'gulp' );
+var rename = require( 'gulp-rename' );
+var print = require( 'gulp-print' );
+
+var styleSRC = './src/scss/style.css';
+var styleDIST = './dist/css/';
+
+gulp.task('style', function(done) {
+    gulp.src(styleSRC)
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(styleDIST))
+    done();
+}); 
