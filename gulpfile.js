14f29dd1a8340a118da77cc19f37889f4359c938
Dmitry <dmitry.oborsky@gmail.com>
2024-06-11 02:03:55 +0300

    first load

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 var gulp = require( 'gulp' );
 var rename = require( 'gulp-rename' );
-var print = require( 'gulp-print' );
+var sass = require( 'gulp-sass' )(require ('sass'));
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
 
-var styleSRC = './src/scss/style.css';
+var styleSRC = 'src/scss/style.scss';
 var styleDIST = './dist/css/';
+var styleWatch = 'src/scss/*.scss'
+var imgSRC ='src/images/*.img'
+var imgDIST ='dist/images/'
 
-gulp.task('style', function(done) {
-    gulp.src(styleSRC)
+gulp.task('style', function() {
+    return gulp.src(styleSRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(styleDIST))
-    done();
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(styleDIST));
 }); 
+
+gulp.task('image', function() {
+    return gulp.src(imgSRC)
+      .pipe(imagemin())
+      .pipe(gulp.dest(imgDIST));
+  });
+
+gulp.task('default', gulp.series('style', 'image'));
+
+gulp.task('watch', function(){
+    gulp.watch(styleWatch, gulp.series('style'));
+    gulp.watch(imgSRC, gulp.series('image'));
+});
