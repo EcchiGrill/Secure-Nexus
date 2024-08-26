0210fc79b14e6aa7c0bf02afd421a625560c0d26
Dmitry <dmitry.oborsky@gmail.com>
2024-08-26 15:19:17 +0300

    README added + some optimization of device compatibility

---
+++ README.md
@@ -0,0 +1,11 @@
+# Secure Nexus
+
+Some layout practising landing
+
+<p align="center">
+  <a href="#">
+    <img alt="/public/screenshot.png" src="/public/screenshot.png">
+  </a>
+</p>
+
+### Stack used: HTML, SASS and Gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,59 @@
-var gulp = require( 'gulp' );
-var rename = require( 'gulp-rename' );
-var sass = require( 'gulp-sass' )(require ('sass'));
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
-var imagemin = require('gulp-imagemin');
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass")(require("sass"));
+var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
 
-var styleSRC = 'src/scss/style.scss';
-var styleDIST = './dist/css/';
-var styleWatch = 'src/scss/*.scss';
-var htmlWatch = '*.html';
-var imgSRC ='src/img/*'
-var imgDIST ='dist/img/'
+var styleSRC = "src/style.scss";
+var styleDIST = "./dist/css/";
+// var styleWatch = "src/scss/*.scss";
+// var htmlWatch = "*.html";
+var imgSRC = "public/*";
+var imgDIST = "dist/img/";
 
-gulp.task('style', function() {
-    return gulp.src(styleSRC)
+gulp.task("style", function () {
+  return gulp
+    .src(styleSRC)
     .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write('./'))
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest(styleDIST));
-}); 
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        chrome: '-browser "chrome.exe"',
-        notify: false
-    })
 });
 
-gulp.task('image', function () {
-        return gulp.src(imgSRC)
-		.pipe(imagemin({}))
-        .pipe(gulp.dest(imgDIST));
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         },
+//         chrome: '-browser "chrome.exe"',
+//         notify: false
+//     })
+// });
+
+gulp.task("image", function () {
+  return gulp.src(imgSRC).pipe(imagemin({})).pipe(gulp.dest(imgDIST));
 });
 
-gulp.task('watch', gulp.parallel('browser-sync', function(done) {
-        gulp.watch(styleWatch, gulp.series('style'));
-        gulp.watch(htmlWatch, browserSync.reload);
-        done();
-    }));
+// gulp.task(
+//   "watch",
+//   gulp.parallel("browser-sync", function (done) {
+//     gulp.watch(styleWatch, gulp.series("style"));
+//     gulp.watch(htmlWatch, browserSync.reload);
+//     done();
+//   })
+// );
 
-
-gulp.task('default', gulp.series('watch'));
+gulp.task("default", gulp.series("style"));
